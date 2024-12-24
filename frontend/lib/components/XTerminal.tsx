d9d8c98a378a29d81ff5829319640882995abddb
Raghav <dev@raghav98.com>
2024-12-24 10:06:51 -0500

    new version that works with xterm

--- frontend/lib/components/XTerminal.tsx
+++ frontend/lib/components/XTerminal.tsx
@@ -1,5 +1,5 @@
-import { Terminal } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
+import { Terminal } from '@xterm/xterm';
+import { FitAddon } from './xterm-addon-fit';
 import './XTerminal.css';
 import { CannonEvent, CannonEventName, TerminalConfig } from './types';
 import { useEffect, useRef, useState } from 'react';
